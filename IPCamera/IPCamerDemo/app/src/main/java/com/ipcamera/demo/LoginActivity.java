c3611e118fdcf124fd2de129d753af837ca2e7d0
luojiahao <710601912@qq.com>
2017-09-22 10:42:36 +0800

    网络摄像机人脸识别

---
+++ IPCamera/IPCamerDemo/app/src/main/java/com/ipcamera/demo/LoginActivity.java
@@ -0,0 +1,174 @@
+package com.ipcamera.demo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.ipcamer.demo.R;
+import com.ipcamera.demo.bean.LoginResponse;
+import com.ipcamera.demo.utils.HttpUtils;
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.Permission;
+import com.yanzhenjie.permission.Rationale;
+import com.yanzhenjie.permission.RationaleListener;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import cn.finalteam.rxgalleryfinal.RxGalleryFinal;
+import cn.finalteam.rxgalleryfinal.imageloader.ImageLoaderType;
+import cn.finalteam.rxgalleryfinal.rxbus.RxBusResultDisposable;
+import cn.finalteam.rxgalleryfinal.rxbus.event.ImageRadioResultEvent;
+import okhttp3.Call;
+
+public class LoginActivity extends Activity implements View.OnClickListener {
+    private static final int REQUEST_PERMISSION_CAMERA =100 ;
+    private Button btn_upLoad;
+    private LinearLayout ll_back;
+    private EditText et_userName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btn_upLoad= (Button) findViewById(R.id.btn_uploadFile);
+        ll_back= (LinearLayout) findViewById(R.id.ll_login_back);
+        et_userName= (EditText) findViewById(R.id.et_userName);
+        ll_back.setOnClickListener(this);
+        btn_upLoad.setOnClickListener(this);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED ){
+            initPermission();
+        }
+    }
+    private void initPermission() {
+        AndPermission.with(getApplicationContext())
+                .requestCode(REQUEST_PERMISSION_CAMERA)
+                .permission(Permission.CAMERA,Permission.PHONE,Permission.MICROPHONE)
+                .callback(this)
+                // rationale作用是：用户拒绝一次权限，再次申请时先征求用户同意，再打开授权对话框；
+                // 这样避免用户勾选不再提示，导致以后无法申请权限。
+                // 你也可以不设置。
+                .rationale(new RationaleListener() {
+                    @Override
+                    public void showRequestPermissionRationale(int requestCode, Rationale rationale) {
+                        // 这里的对话框可以自定义，只要调用rationale.resume()就可以继续申请。
+                        AndPermission.rationaleDialog(LoginActivity.this, rationale).show();
+                    }
+                })
+                .start();
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_login_back:
+                finish();
+                break;
+            case R.id.btn_uploadFile:
+                final String userName=et_userName.getText().toString().trim();
+                if(userName.equals("")){
+                    Toast.makeText(this,"用户名不能为空",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                //自定义方法的单选
+                RxGalleryFinal
+                        .with(this)
+                        .image()
+                        .radio()
+                        .imageLoader(ImageLoaderType.PICASSO)
+                        .subscribe(new RxBusResultDisposable<ImageRadioResultEvent>() {
+                            @Override
+                            protected void onEvent(ImageRadioResultEvent imageRadioResultEvent) throws Exception {
+                                //图片选择结果
+                                String  imgFilePath=imageRadioResultEvent.getResult().getOriginalPath();
+                                postFile(imgFilePath,userName);
+                            }
+
+                        })
+                        .openGallery();
+                break;
+        }
+    }
+    private void postFile(String path,String userName) {
+        File file = new File(path);
+//        if (file != null) {
+//            HttpUtils.sendVeriflyPictrue("/distinguish", file, "罗嘉豪", new StringCallback() {
+//                @Override
+//                public void onError(Call call, Exception e, int id) {
+//                    Log.e("response", e.toString());
+//                }
+//
+//                @Override
+//                public void onResponse(String response, int id) {
+//                    Gson gson = new Gson();
+//                    Type type = new TypeToken<LoginResponse>() {
+//                    }.getType();
+//                    LoginResponse loginResponse = gson.fromJson(response, type);
+//                    if (loginResponse.getData().equals("200")) {
+//                        Toast.makeText(LoginActivity.this, loginResponse.getMsg(), Toast.LENGTH_LONG).show();
+//                    } else {
+//                    }
+//                }
+//            });
+//        }
+//    }
+        if(file!=null) {
+            HttpUtils.sendLoginPictrue("/register", file, userName, new StringCallback() {
+                @Override
+                public void onError(Call call, Exception e, int id) {
+                    Log.e("response", e.toString());
+                }
+
+                @Override
+                public void onResponse(String response, int id) {
+                    Gson gson = new Gson();
+                    Type type = new TypeToken<LoginResponse>() {
+                    }.getType();
+                    LoginResponse loginResponse = gson.fromJson(response, type);
+                    if (loginResponse.getData().equals("200")) {
+                        Toast.makeText(LoginActivity.this, loginResponse.getMsg(), Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(LoginActivity.this, loginResponse.getMsg(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * 图片装换成byte数组
+     * @param imgPath
+     * @return
+     */
+    public static byte[] imageToByteArray(String imgPath) {
+        BufferedInputStream in;
+        try {
+            in = new BufferedInputStream(new FileInputStream(imgPath));
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            int size = 0;
+            byte[] temp = new byte[1024];
+            while((size = in.read(temp))!=-1) {
+                out.write(temp, 0, size);
+            }
+            in.close();
+            return out.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ IPCamera/IPCamerDemo/app/src/main/java/com/ipcamera/demo/service/MsgService.java
@@ -0,0 +1,69 @@
+package com.ipcamera.demo.service;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+
+import com.ipcamera.demo.bean.MessageEvent;
+
+import org.greenrobot.eventbus.EventBus;
+
+/**
+ * Created by Min on 2017/9/5.
+ */
+
+public class MsgService extends Service {
+private  WebView webView;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        webView=new WebView(getApplicationContext());
+        initWebView();
+        //只在service创建的时候调用一次，可以在此进行一些一次性的初始化操作
+    }
+    private void initWebView() {
+        webView.setVerticalScrollbarOverlay(true);
+        //设置webview支持JS
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDefaultTextEncodingName("utf-8");
+        webView.addJavascriptInterface(new JsInterface(getApplicationContext()),"webview");
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //当其他组件调用startService()方法时，此方法将会被调用
+        //在这里进行这个service主要的操作
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        //当其他组件调用bindService()方法时，此方法将会被调用
+        //如果不想让这个service被绑定，在此返回null即可
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        //service调用的最后一个方法
+        //在此进行资源的回收
+        webView.destroy();
+        super.onDestroy();
+    }
+    private class JsInterface{
+        private Context context;
+        public JsInterface(Context mcontext){
+            this.context=mcontext;
+        }
+        @JavascriptInterface
+        public void acceptMsg(String msg){
+            EventBus.getDefault().post(new MessageEvent(msg));
+        }
+    }
+}
